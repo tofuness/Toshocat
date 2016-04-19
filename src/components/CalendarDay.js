84d6918c160c76b1ab38d97be99f64f91bba3304
Desuvader <desuvader@gmail.com>
2016-04-19 08:08:47 +0200

    Minor tweak to calendar design

--- src/components/CalendarDay.js
+++ src/components/CalendarDay.js
@@ -36,6 +36,7 @@ class CalendarDay extends Component {
               if (
                 [
                   'Mayoiga',
+                  'Kuma Miko: Girl Meets Bear',
                   'JoJo no Kimyou na Bouken: Diamond wa Kudakenai',
                   'Ansatsu Kyoushitsu (TV) 2nd Season',
                   'Boku no Hero Academia',
