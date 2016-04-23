3da671cd9f1d41054d4fc530f231d559666a8e44
Desuvader <desuvader@gmail.com>
2016-04-23 07:53:22 +0200

    Update sidebar components to stateless

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import LogoContainer from '../containers/LogoContainer';
 import SideBarNavigation from './SideBarNavigation';
 
-class SideBar extends Component {
-  render() {
-    return (
-      <div className="sidebar">
-        <LogoContainer />
-        <SideBarNavigation />
-      </div>
-    );
-  }
-}
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <LogoContainer />
+      <SideBarNavigation />
+    </div>
+  );
+};
 
 export default SideBar;
--- src/components/SideBarNavigation.js
+++ src/components/SideBarNavigation.js
@@ -1,54 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 
-class SideBarNavigation extends Component {
-  render() {
-    return (
-      <div className="sidebar-navigation">
-        <div className="sidebar-navigation-label">
-          Main
-        </div>
-        <div className="sidebar-navigation-link">
-          <Link to="/animelist" activeClassName="active">
-            <span className="icon-list-unordered"></span>Anime list
-          </Link>
-        </div>
-        <div className="sidebar-navigation-link">
-          <Link to="/mangalist" activeClassName="active">
-            <span className="icon-list-unordered"></span>Manga list
-          </Link>
-        </div>
-        <div className="sidebar-navigation-spacer"></div>
-        <div className="sidebar-navigation-label">
-          Browse
-        </div>
-        <div className="sidebar-navigation-link">
-          <Link to="/calendar" activeClassName="active">
-            <span className="icon-calendar"></span>Calendar
-          </Link>
-        </div>
-        <div className="sidebar-navigation-link">
-          <Link to="/rss" activeClassName="active">
-            <span className="icon-rss"></span>RSS Feed
-          </Link>
-        </div>
-        <div className="sidebar-navigation-link">
-          <Link to="/search" activeClassName="active">
-            <span className="icon-magnifying-glass"></span>Search
-        </Link>
-        </div>
-        <div className="sidebar-navigation-spacer"></div>
-        <div className="sidebar-navigation-label">
-          System
-        </div>
-        <div className="sidebar-navigation-link">
-          <Link to="/settings" activeClassName="active">
-            <span className="icon-gear"></span>Settings
-          </Link>
-        </div>
+const SideBarNavigation = () => {
+  return (
+    <div className="sidebar-navigation">
+      <div className="sidebar-navigation-label">
+        Main
       </div>
-    );
-  }
-}
+      <div className="sidebar-navigation-link">
+        <Link to="/animelist" activeClassName="active">
+          <span className="icon-list-unordered"></span>Anime list
+        </Link>
+      </div>
+      <div className="sidebar-navigation-link">
+        <Link to="/mangalist" activeClassName="active">
+          <span className="icon-list-unordered"></span>Manga list
+        </Link>
+      </div>
+      <div className="sidebar-navigation-spacer"></div>
+      <div className="sidebar-navigation-label">
+        Browse
+      </div>
+      <div className="sidebar-navigation-link">
+        <Link to="/calendar" activeClassName="active">
+          <span className="icon-calendar"></span>Calendar
+        </Link>
+      </div>
+      <div className="sidebar-navigation-link">
+        <Link to="/rss" activeClassName="active">
+          <span className="icon-rss"></span>RSS Feed
+        </Link>
+      </div>
+      <div className="sidebar-navigation-link">
+        <Link to="/search" activeClassName="active">
+          <span className="icon-magnifying-glass"></span>Search
+      </Link>
+      </div>
+      <div className="sidebar-navigation-spacer"></div>
+      <div className="sidebar-navigation-label">
+        System
+      </div>
+      <div className="sidebar-navigation-link">
+        <Link to="/settings" activeClassName="active">
+          <span className="icon-gear"></span>Settings
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default SideBarNavigation;
