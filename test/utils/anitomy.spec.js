506667adaf7f953a078bb19e166944f4573c055d
Desuvader <desuvader@gmail.com>
2016-04-24 18:02:06 +0200

    Skip anitomy tests for non-windows platforms
    Need to compile binary to other platforms before running these

--- test/utils/anitomy.spec.js
+++ test/utils/anitomy.spec.js
@@ -1,9 +1,11 @@
 import { expect } from 'chai';
 import anitomy from '../../src/utils/anitomy';
+import os from 'os';
 
 describe('anitomy', () => {
   describe('parse', () => {
-    it('should parse one filename', (done) => {
+    it('should parse one filename', function(done) {
+      if (os.platform() !== 'win32') this.skip();
       anitomy.parse('[HorribleSubs] Kono Subarashii Sekai ni Shukufuku wo! - 04 [1080p].mkv',
         (parsedData) => {
           expect(parsedData).to.be.an('array');
@@ -13,6 +15,7 @@ describe('anitomy', () => {
       );
     });
     it('should parse multiple filenames', (done) => {
+      if (os.platform() !== 'win32') this.skip();
       anitomy.parse([
         '[HorribleSubs] Kono Subarashii Sekai ni Shukufuku wo! - 04 [1080p].mkv',
         '[KappaSubs] Kono Subarashii Sekai ni Shukufuku wo! - 03 [1080p].mkv'
@@ -24,6 +27,7 @@ describe('anitomy', () => {
       });
     });
     describe('should not parse anything when', () => {
+      if (os.platform() !== 'win32') this.skip();
       it('filename is undefined', (done) => {
         anitomy.parse(undefined, (parsedData) => {
           expect(parsedData).to.equal(false);
@@ -31,12 +35,14 @@ describe('anitomy', () => {
         });
       });
       it('filename is null', (done) => {
+        if (os.platform() !== 'win32') this.skip();
         anitomy.parse(null, (parsedData) => {
           expect(parsedData).to.equal(false);
           done();
         });
       });
       it('filename is empty array', (done) => {
+        if (os.platform() !== 'win32') this.skip();
         anitomy.parse([], (parsedData) => {
           expect(parsedData).to.equal(false);
           done();
